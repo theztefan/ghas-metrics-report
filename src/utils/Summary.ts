b3055fc472ebd4474d515892e0ced4fa1fbce7c7
Stefan Petrushevski <theztefan@github.com>
2022-12-13 17:59:57 +0100

    Summary table fix

--- src/utils/Summary.ts
+++ src/utils/Summary.ts
@@ -6,17 +6,15 @@ export function prepareSummary(report: Report): void {
   core.summary.addHeading("GHAS Metrics Summary");
   core.summary.addBreak();
 
-  const dependabotTop10rows = report.dependabot_metrics?.top10.map((a: any) => [
-    a.security_vulnerability?.package.name,
-    a.security_vulnerability?.severity,
-    a.security_vulnerability?.vulnerable_version_range,
-    a.security_vulnerability?.first_patched_version?.identifier,
-    a.security_advisory?.cve_id,
-    a.security_advisory?.cvss?.vector_string,
-  ]);
-
-  core.debug("#####");
-  core.debug(dependabotTop10rows);
+  const dependabotTop10rows: SummaryTableRow =
+    report.dependabot_metrics?.top10.map((a: any) => [
+      a.security_vulnerability?.package.name,
+      a.security_vulnerability?.severity,
+      a.security_vulnerability?.vulnerable_version_range,
+      a.security_vulnerability?.first_patched_version?.identifier,
+      a.security_advisory?.cve_id,
+      a.security_advisory?.cvss?.vector_string,
+    ]);
 
   const codeScanningTop10rows: SummaryTableRow =
     report.code_scanning_metrics?.top10.map((a: any) => [
@@ -45,97 +43,7 @@ export function prepareSummary(report: Report): void {
     ])
     .addBreak()
     .addHeading("Dependabot - Top 10", 2)
-    .addTable([
-      [
-        "Package",
-        "Severity",
-        "Vulnerable versions",
-        "Patched version",
-        "CVE",
-        "CVSS",
-      ],
-      //dependabotTop10rows,
-      [
-        "loader-utils",
-        "critical",
-        ">= 2.0.0, < 2.0.3",
-        "2.0.3",
-        "CVE-2022-37601",
-        "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
-      ],
-      [
-        "loader-utils",
-        "critical",
-        "< 1.4.1",
-        "1.4.1",
-        "CVE-2022-37601",
-        "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
-      ],
-      [
-        "shell-quote",
-        "critical",
-        "<= 1.7.2",
-        "1.7.3",
-        "CVE-2021-42740",
-        "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
-      ],
-      [
-        "eventsource",
-        "critical",
-        "< 1.1.1",
-        "1.1.1",
-        "CVE-2022-1650",
-        "CVSS:3.1/AV:N/AC:L/PR:N/UI:R/S:C/C:H/I:H/A:N",
-      ],
-      [
-        "ejs",
-        "critical",
-        "< 3.1.7",
-        "3.1.7",
-        "CVE-2022-29078",
-        "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
-      ],
-      [
-        "url-parse",
-        "critical",
-        "< 1.5.8",
-        "1.5.8",
-        "CVE-2022-0686",
-        "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:N",
-      ],
-      [
-        "json-schema",
-        "critical",
-        "< 0.4.0",
-        "0.4.0",
-        "CVE-2021-3918",
-        "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
-      ],
-      [
-        "pycrypto",
-        "critical",
-        "<= 2.6.1",
-        "xx",
-        "CVE-2013-7459",
-        "CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
-      ],
-      [
-        "qs",
-        "high",
-        ">= 6.5.0, < 6.5.3",
-        "6.5.3",
-        "CVE-2022-24999",
-        "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:N/A:H",
-      ],
-      [
-        "qs",
-        "high",
-        ">= 6.7.0, < 6.7.3",
-        "6.7.3",
-        "CVE-2022-24999",
-        "CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:N/A:H",
-      ],
-    ])
+    .addTable([dependabotTop10rows])
 
     .addBreak()
     .addHeading("Code Scanning")
@@ -145,10 +53,8 @@ export function prepareSummary(report: Report): void {
       `Fixed in the past 7 days: ${report.code_scanning_metrics?.fixedLastWeek}`,
       `MTTR: ${report.code_scanning_metrics?.mttr.mttr}`,
     ])
-    .addTable([
-      ["Vulnerability", "Severity", "Tool", "Vulnerable file", "Link"],
-      codeScanningTop10rows,
-    ])
+    .addHeading("Code Scanning - Top 10", 2)
+    .addTable([codeScanningTop10rows])
 
     .addBreak()
     .addHeading("Secret Scanning")
@@ -158,6 +64,7 @@ export function prepareSummary(report: Report): void {
       `Fixed in the past 7 days: ${report.secret_scanning_metrics?.fixedLastWeek}`,
       `MTTR: ${report.secret_scanning_metrics?.mttr.mttr}`,
     ])
+    .addHeading("Secret Scanning - Top 10", 2)
     .addTable([
       ["Secret Type", "Found at", "Push Protection Bypass", "Link"],
       secretScanningTop10rows,
