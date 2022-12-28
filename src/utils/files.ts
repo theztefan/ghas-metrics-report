b946a5dd02c157ff2dba12768d1d7fff36e3f249
Stefan Petrushevski <theztefan@github.com>
2022-12-28 18:46:24 +0100

    write report to workspace path

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -4,7 +4,7 @@ import jsPDF from "jspdf";
 import { join } from "path";
 
 export const syncWriteFile = (filename: string, data: any): void => {
-  const outputFilename = join(__dirname, filename);
+  const outputFilename = join(process.env.GITHUB_WORKSPACE as string, filename);
   writeFileSync(outputFilename, data, {
     flag: "w",
   });
@@ -16,8 +16,8 @@ export const preparePdfAndWriteToFile = (
   filename: string,
   report: jsPDF
 ): void => {
-  const outputFilename = join(__dirname, filename);
-
+  //const outputFilename = join(__dirname, filename);
+  const outputFilename = join(process.env.GITHUB_WORKSPACE as string, filename);
   report.save(outputFilename);
   core.debug(`[📝] File ${outputFilename} written`);
   return;
