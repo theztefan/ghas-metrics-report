eb0f1c53f99b53bdf0a757736d3895be07f75146
Chad Bentz <1760475+felickz@users.noreply.github.com>
2025-01-16 16:39:27 +0000

    compile

--- dist/index.js
+++ dist/index.js
@@ -64486,20 +64486,20 @@ function compareAlertSeverity(a, b) {
     const weight = {
         critical: 7,
         high: 6,
-        medium: 5,
-        low: 4,
+        error: 5,
+        medium: 4,
         warning: 3,
-        note: 2,
-        error: 1,
+        low: 2,
+        note: 1,
         none: 0,
     };
     const severity1 = getAlertSeverity(a);
     const severity2 = getAlertSeverity(b);
-    if (weight[severity1] < weight[severity2]) {
-        return 1;
-    }
-    else if (weight[severity1] > weight[severity2]) {
+    if (weight[severity1] > weight[severity2]) {
         return -1;
+    }
+    else if (weight[severity1] < weight[severity2]) {
+        return 1;
     }
     return 0;
 }
